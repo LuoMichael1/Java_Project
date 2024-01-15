a42e4c53d9eefb5ef45c341cb6059b6fea5cf1ac
Alec Li <alec.li@student.tdsb.on.ca>
2024-01-15 17:52:54 -0500

    Remove unused imports

--- tile_game/Chest.java
+++ tile_game/Chest.java
@@ -5,7 +5,6 @@ import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
--- tile_game/Vent.java
+++ tile_game/Vent.java
@@ -4,7 +4,6 @@ import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
